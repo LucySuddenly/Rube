30879244eb72cacd1a56853d8b5de4325600c304
LucySuddenly <lucy.suddenly@outreach.io>
2020-10-21 16:54:01 -0700

    structure && task

---
+++ README.md
@@ -0,0 +1,4 @@
+#RUBE
+Cool.... but what does it do?
+
+️🚧 ⚠️ under construction (so I'm not really sure yet honestly)
---
+++ models/task.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"errors"
+	"time"
+)
+
+type taskType string
+
+const (
+	wait = "wait"
+)
+
+type task struct {
+	Duration time.Time
+	Type taskType
+}
+
+func (t task) validate() error {
+	switch t.Type {
+	case wait:
+	default:
+		return errors.New("invalid task type")
+	}
+	return nil
+}
